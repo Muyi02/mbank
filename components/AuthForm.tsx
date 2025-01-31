6b0aefb009be30bffd1e2983b8992fac7844a4f3
Muyi02 <muyiidm@gmail.com>
2025-01-31 15:09:06 +0000

    Starting form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,10 +3,48 @@ import  Link from 'next/link'
 import Image from 'next/image'
 import React, {useState} from 'react'
 
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+})
+
+
 
 
 const AuthForm = ({type}: {type: string}) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+
+     // 1. Define your form.
+        const form = useForm<z.infer<typeof formSchema>>({
+            resolver: zodResolver(formSchema),
+            defaultValues: {
+            username: "",
+            },
+        })
+        
+        // 2. Define a submit handler.
+        function onSubmit(values: z.infer<typeof formSchema>) {
+            // Do something with the form values.
+            // ✅ This will be type-safe and validated.
+            console.log(values)
+        }
+    
  
     return (
     <section className='auth-form'>
@@ -28,11 +66,46 @@ const AuthForm = ({type}: {type: string}) => {
                   : 'Sign Up'
                  }
                  <p className='text-16 font-normal text-gray-600'>
-
+                    {user
+                    ? 'Link your account to get started'
+                    : 'Please enter your details'}
                  </p>
                </h1>
             </div>
         </header>
+        {user ?(
+            <div className='flex flex-col gap-4'>
+               {/*PlaidLink */}
+            </div>
+        ): (
+            <>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <div className = 'form-item'>
+                               <FormLabel className='form-label'>
+                                Email
+                               </FormLabel>
+                               <div className='flex w-full flex-col'>
+                                  <FormControl>
+                                    <Input
+                                    placeholder='Enter your email'
+                                    className='input-class'
+                                    />
+                                  </FormControl>
+
+                               </div>
+                            </div>
+                        )}
+                        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+            </>
+        )}
     </section>
   )
 }
